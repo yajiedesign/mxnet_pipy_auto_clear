127d9eb4f5c471549dc4a2e031b87f38e3e3332b
yajiedesign <yajiedesign@gmail.com>
2019-03-20 18:21:03 +0800

    first add

---
+++ do_del.py
@@ -0,0 +1,56 @@
+import requests
+from lxml import etree
+
+from get_want_del import project_list
+
+
+def del_one_release(s, project, version):
+    release_url = "https://pypi.org/manage/project/{0}/release/{1}/".format(project, version)
+    release_first = s.get(release_url)
+    if release_first.status_code != 200:
+        print("get release error {0}/{1}".format(project, version))
+        return
+    release_first_html = etree.HTML(release_first.content)
+    csrf_token = release_first_html.xpath('//*[@id="delete-release-modal"]/div/form/input')[0].attrib["value"]
+    post_data = {"csrf_token": csrf_token, "confirm_version": version}
+    rs = s.post(release_url, post_data, headers={'referer': release_url})
+    if rs.status_code == 200:
+        print("delete success {0}/{1}".format(project, version))
+    else:
+        print("delete fail {0}/{1}".format(project, version))
+
+
+def del_one_project(s, project):
+    with open("want_del/{0}.txt".format(project), "r", encoding="utf-8") as obj:
+        for line in obj:
+            sp = line.strip().split(" ")
+            if sp[1] == "delete":
+                del_one_release(s, project, sp[0])
+
+
+def main():
+    s = requests.session()
+    login_url = 'https://pypi.org/account/login/'
+
+    login_first = s.get(login_url)
+    login_first_html = etree.HTML(login_first.content)
+    csrf_token = login_first_html.xpath('//*[@id="content"]/section/div/form/input')[0].attrib["value"]
+
+    with open("setting.txt","r",encoding="utf-8") as obj:
+        lines = obj.readlines()
+        
+    post_data = {"csrf_token": csrf_token, "username": lines[0].strip(),
+                 "password": lines[1].strip()}
+    rs = s.post(login_url, post_data, headers={'referer': login_url})
+    if rs.status_code != 200:
+        print("login error")
+        return
+
+    for project in project_list:
+        del_one_project(s, project)
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ get_want_del.py
@@ -0,0 +1,76 @@
+import datetime
+import json
+import os
+import re
+
+import requests
+
+project_list = ["mxnet"]
+
+
+def main():
+    os.makedirs("want_del", exist_ok=True)
+
+    for project in project_list:
+        one_project(project)
+    pass
+
+
+def one_project(project_name):
+    must_find = ["win_amd64", "manylinux1_x86_64", "macosx"]
+    mxnet_json_str = requests.get('https://pypi.org/pypi/{0}/json'.format(project_name))
+    mxnet_json = json.loads(mxnet_json_str.text)
+    releases = mxnet_json["releases"]
+    bucket = {}
+    bucket_list = []
+    today = datetime.datetime.today()
+    for release_k in releases:
+        match_obj = re.match(".*?b([0-9]{8})", release_k, flags=0)
+        if not match_obj:
+            continue
+        date = datetime.datetime.strptime(match_obj.group(1), "%Y%m%d")
+        year_week = (date.isocalendar()[0], date.isocalendar()[1])
+        if year_week not in bucket:
+            bucket[year_week] = []
+            bucket_list.append(year_week)
+        bucket[year_week].append((date, release_k))
+
+    with open("want_del/{0}.txt".format(project_name), "w", encoding="utf-8") as obj:
+        for year_week in bucket_list:
+            data_first_week = datetime.datetime.strptime('{0} {1} 1'.format(*year_week), '%G %V %u')
+            if (today - data_first_week).days <= 97:
+                continue
+
+            year_week_list = bucket[year_week]
+            if len(year_week_list) == 1:
+                obj.write("{0} keep\n".format(year_week_list[0][1]))
+                continue
+            year_week_list.sort(key=lambda e: e[0])
+            out_list = []
+            is_find_keep = False
+            for item in reversed(year_week_list):
+                if is_find_keep:
+                    out_list.append((item[1], "delete"))
+                    continue
+                release = releases[item[1]]
+                find_set = set()
+                for release_item in release:
+                    filename = release_item["filename"]
+                    for f in must_find:
+                        if f in filename:
+                            find_set.add(f)
+                if len(find_set) == len(must_find):
+                    is_find_keep = True
+                    out_list.append((item[1], "keep"))
+                else:
+                    out_list.append((item[1], "delete"))
+            if not is_find_keep:
+                for i in reversed(out_list):
+                    obj.write("{0} error\n".format(i[0]))
+            else:
+                for i in reversed(out_list):
+                    obj.write("{0} {1}\n".format(*i))
+
+
+if __name__ == '__main__':
+    main()
